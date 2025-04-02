b1f1bfef3a18b568faaedfcac388a7e55de0cb86
Rachel H Barcellona <rbarcellona@tampabay.rr.com>
2025-04-01 22:08:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,67 +16,68 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method")); 
 app.use(morgan("dev")); 
 
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 
-//const Song = require('./models/songs.js')
+const Song = require('./models/songs.js')
 
-//app.get('/Songs:songId/edit', async (req, res)=>{
-  //const foundSongs = await Songs.findById(req.params.SongsId)
-  //res.render('songs/edit.ejs')
-  //Song: foundSongs
-//})
+app.get('/songs/:songId/edit', async (req, res)=>{
+  const foundSongs = await Songs.findById(req.params.songId)
+  res.render('/songs/edit.ejs'), {
+    song:foundSongs
+  }
+})
 
-//app.put('/songs/:songId', async (req, res)=>{
-  //if(req.body.isASong === 'on') {
-    //req.body.isASong = true
- // } else {
-   // req.body.isaSong = false
- // }
-  //await Song.findByIdAndUpdate(req.params.songId, req.body)
-  //res.redirect('/songs')
-//})
+app.put('/songs/:songId', async (req, res)=>{
+  if(req.body.isASong === 'on') {
+   req.body.isASong = true
+  } else {
+    req.body.isaSong = false
+  }
+  await Song.findByIdAndUpdate(req.params.songId, req.body)
+  res.redirect('/songs')
+})
 
 app.get('/', async (req, res)=>{
   res.render('index.ejs')
 })
 
-//app.delete("/songs/:songsId", async (req, res) => {
-  //await Songs.findByIdAndDelete(req.params.fruitId);
-  //res.redirect("/songs");
-//});
+app.delete("/songs/:songId", async (req, res) => {
+  await Songs.findByIdAndDelete(req.params.songId);
+  res.redirect("/songs");
+});
 
-//app.get('/songs', async (req, res)=> {
-  //const allSongs = await Song.find()
-  //res.render('songs/index.ejs' , {
-   // Song: allSongs
- // })
-//})
-
-//app.get("/songs/:songsId", async (req, res) => {
-  //const foundSongs = await Songs.findById(req.params.songsId);
- //res.render("songs/show.ejs", { songs: foundSongs });
-//});
-
-//app.post('/songs', async (req, res)=>{
-//if (req.body.isASong === "on") {
-    //req.body.isASong = true;
- // } else {
-    //req.body.isASong = false
- // }
- // await Song.create(req.body);
- // res.redirect("/songs/");
-//});
-
-app.get('songs/new', (req, res) =>{
-  res.render('/songs/new.ejs')
+app.get('/songs', async (req, res)=> {
+  const allSongs = await Song.find()
+  console.log(allSongs)
+  res.render('songs/index.ejs' , {
+    songs: allSongs
+  })
 })
+
+app.post('/songs', async (req, res)=>{
+if (req.body.isASong === "on") {
+    req.body.isASong = true;
+  } else {
+    req.body.isASong = false
+  }
+  await Song.create(req.body);
+  res.redirect("/songs");
+});
+
+app.get('/songs/new', (req, res) =>{
+  res.render('songs/new.ejs')
+})
+
+app.get("/songs/:songId", async (req, res) => {
+  const foundSongs = await Song.findById(req.params.songId);
+  console.log(foundSongs)
+  res.render("songs/show.ejs", { song: foundSongs });
+});
 
 app.listen(3000, ()=>{
 console.log('Listening on port 3000')
 })
-
-
 
 
 
